3a8d99b297f56659b8d80eeeec04108dabc4f96f
UbeL13 <u.b.2004@yandex.ru>
2023-11-08 08:51:11 +0400

    read csv

--- first.py
+++ first.py
@@ -53,6 +53,16 @@ def write_to_csv(data_from_table):
             file_writer = csv.writer(csvfile, delimiter=",", lineterminator="\r") 
             file_writer.writerow([item["data"], item["temp_morning"], item["presure_morning"], item["wind_morning"], item["temp_evening"], item["presure_evening"], item["wind_evening"]]) 
 
+def read_csv_and_write_to_new_files():
+    with open('dataset.csv', newline='') as f: 
+        fieldnames = ['data', 'temp_morning', 'presure_morning', 'wind_morning', 'temp_evening', 'presure_evening', 'wind'] 
+        reader = csv.DictReader(f, fieldnames=fieldnames) 
+        for row in reader: 
+            file_writer = csv.writer(open('dataset-number.csv', 'a', newline=''), lineterminator="\r") 
+            file_writer.writerow([row['data']]) 
+            file_writer = csv.writer(open('dataset-meteodate.csv', 'a', newline=''), lineterminator="\r") 
+            file_writer.writerow([row['temp_morning'], row['presure_morning'], row['wind_morning'], row['temp_evening'], row['presure_evening'], row['wind']])
+
 
 for year in years: 
     for month in months:
@@ -66,7 +76,6 @@ for year in years:
         except Exception: 
             continue
 
-
         soup = BeautifulSoup(src, "lxml") 
         try: 
             tab = soup.find("table").find("tbody").find_all("tr") 
@@ -74,3 +83,6 @@ for year in years:
             continue
 
         data_from_table = get_data_from_table(tab, year, month)
+        write_to_csv(data_from_table)
+
+read_csv_and_write_to_new_files() 
